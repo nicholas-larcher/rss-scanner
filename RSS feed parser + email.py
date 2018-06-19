424637a908b08ccdca6c4690929872fa92577b35
nicholas-larcher <40302942+nicholas-larcher@users.noreply.github.com>
2018-06-19 12:48:13 -0400

    Update RSS feed parser + email.py

--- RSS feed parser + email.py
+++ RSS feed parser + email.py
@@ -1,7 +1,7 @@
 # library imports
 import feedparser
-# import re
-# import smtplib
+import re
+import smtplib
 import sqlite3
 import time
 from datetime import date
@@ -11,10 +11,11 @@ db = sqlite3.connect('nvd.db')
 c = db.cursor()
 
 # remove create table after running once
-c.execute('''CREATE TABLE nvd_table (
-                        hyperlink text,
-                        product text,
-                        date_added text)''')
+
+# c.execute('''CREATE TABLE nvd_table (
+#                       hyperlink text,
+#                       product text,
+#                       date_added text)''')
 
 # NVD RSS feed variable
 d = feedparser.parse('https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml')
@@ -23,6 +24,8 @@ print(d['feed']['title'], 'Scanner')
 print("+++++++++++++++++++++++++++++++++++++++\n")
 
 # function for iterating through the entries and printing out how many vulns there are
+
+
 def product_scan(product_name):
 
     # vulnerability links list
@@ -58,6 +61,5 @@ product_list = ['mysql', 'windows', 'linux', 'explorer', 'php', 'webex', 'firefo
 for product in product_list:
     product_scan(product)
 
-print(db.execute('''SELECT * FROM nvd_table;'''))
-
+db.commit()
 db.close()
