5055d6a61e74c060427efcff8da33f514c61b99a
nicholas-larcher <40302942+nicholas-larcher@users.noreply.github.com>
2018-06-21 12:08:07 -0400

    Update and rename RSS feed parser + email.py to nvd.py

--- nvd.py
+++ nvd.py
@@ -9,18 +9,7 @@ from datetime import date
 # SQLite databases variable & cursor
 db = sqlite3.connect('nvd.db')
 c = db.cursor()
-db2 = sqlite3.connect('product.db')
-c2 = db2.cursor()
 
-# comment out create table after running once
-c.execute('''CREATE TABLE nvd_table (
-                        hyperlink text,
-                        product text,
-                        date_added text)''')
-
-c2.execute('''CREATE TABLE products_table (
-                       product text,
-                       date_added text)''')
 
 # NVD RSS feed variable
 d = feedparser.parse('https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml')
@@ -47,9 +36,8 @@ def product_scan(product_name):
             vuln_list.append((entry.link, entry.title))
             title_list.append([entry.title])
 
-# sql code for inserting stuff into tables
-    c2.executemany('INSERT INTO products_table VALUES (?, CURRENT_DATE)', title_list)
-    c.executemany('INSERT INTO nvd_table VALUES (?, ?, CURRENT_DATE)', vuln_list)
+    # sql insert. there will be no duplicates
+    #c.executemany('INSERT OR REPLACE INTO cve VALUES (?, ?, CURRENT_DATE)', vuln_list)
 
     # making it look nice
     if count == 1:
@@ -65,12 +53,16 @@ def product_scan(product_name):
     for x in vuln_list:
         print(x)
 
+# calling the function and searching for vulns based on keyword(s) in database
 
-# calling the function and searching for vulns based on keyword(s)
 product_list = ['mysql', 'windows', 'linux', 'explorer', 'php', 'webex', 'firefox', 'norton', 'mcafee', 'symantec']
+#c.execute('SELECT product FROM product')
+#product_list = c.fetchall()
+
+
+
 for product in product_list:
     product_scan(product)
-db2.commit()
-db2.close()
+
 db.commit()
 db.close()
