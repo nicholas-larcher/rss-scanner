9952b2e3f221006e2a2485f9058fc8d8158408ba
nicholas-larcher <40302942+nicholas-larcher@users.noreply.github.com>
2018-06-26 11:32:54 -0400

    Update nvd.py

--- nvd.py
+++ nvd.py
@@ -11,22 +11,39 @@ db = sqlite3.connect('nvd.db')
 c = db.cursor()
 
 
+# comment out create table after running once
+
+#c.execute('''CREATE TABLE nvd_table (
+#                        hyperlink text,
+#                        product text,
+#                        date_added text)''')
+
+#c2.execute('''CREATE TABLE products_table (
+#                       product text,
+#                       date_added text)''')
+
 # NVD RSS feed variable
 d = feedparser.parse('https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss-analyzed.xml')
 print("\n+++++++++++++++++++++++++++++++++++++++")
 print(d['feed']['title'], 'Scanner')
 print("+++++++++++++++++++++++++++++++++++++++\n")
 
+# function for database product names instead of hardcoding names in the code.
+def read_from_db():
+
+    read_from_db_list = []
+    c.execute('SELECT product_name FROM product')
+    for row in c.fetchall():
+        read_from_db_list.append(row[0])
+
+    return read_from_db_list
+
 # function for iterating through the entries and printing out how many vulns there are
-
-
 def product_scan(product_name):
 
     # vulnerability links list
     vuln_list = []
     title_list =[]
-
-
     # counter for how many vulns per product
     count = 0
     for entry in d.entries:
@@ -36,8 +53,10 @@ def product_scan(product_name):
             vuln_list.append((entry.link, entry.title))
             title_list.append([entry.title])
 
-    # sql insert. there will be no duplicates
-    #c.executemany('INSERT OR REPLACE INTO cve VALUES (?, ?, CURRENT_DATE)', vuln_list)
+# sql code for inserting stuff into tables
+# the 'ignore' part will avoid adding duplicates so long as there is a UNIQUE column.
+
+        c.executemany('INSERT OR IGNORE INTO cve VALUES (?, ?, CURRENT_DATE)', vuln_list)
 
     # making it look nice
     if count == 1:
@@ -53,16 +72,10 @@ def product_scan(product_name):
     for x in vuln_list:
         print(x)
 
-# calling the function and searching for vulns based on keyword(s) in database
 
-product_list = ['mysql', 'windows', 'linux', 'explorer', 'php', 'webex', 'firefox', 'norton', 'mcafee', 'symantec']
-#c.execute('SELECT product FROM product')
-#product_list = c.fetchall()
-
-
-
-for product in product_list:
+for product in read_from_db():
     product_scan(product)
+
 
 db.commit()
 db.close()
