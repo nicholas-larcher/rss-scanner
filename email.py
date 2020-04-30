80e59a3e771bd198e4e547f2eb5647c5e1e2377c
Nicholas Larcher <40302942+nicholas-larcher@users.noreply.github.com>
2020-04-30 08:03:03 -0400

    Update email.py

--- email.py
+++ email.py
@@ -9,38 +9,13 @@ def send_email(to_addr, vuln_desc, cve_link_string):
 
     msg = MIMEMultipart()
     # sender email, password, receiver email and subject.
-    msg['From'] = 'oag.bvg.test@gmail.com'
-    password = '240Sparks'
+    msg['From'] = #enter email
+    password = #enter password
     msg['To'] = to_addr
-    msg['Subject'] = 'IT Security automated vulnerability monitoring / ' \
-                     'Système automatisé de surveillance des vulnérabilitées de Sécurité TI'
+    msg['Subject'] = #subject message
 
     # body of email, string mixed with HTML
-    body =  "Le francais suit l'anglais" \
-            "<br><br>" \
-            "Hello, there is a new vulnerability for your department." \
-            "<br> " \
-            "Summary of the vulnerability:"\
-            "<br>"\
-            "<strong>" + vuln_desc + "</strong>" \
-            "<br>"\
-            "<a href="+cve_link_string+">Clink this link to view the vulnerability</a>" \
-            "<br>" \
-            "This is an automated message, please do not respond. If you have any questions or concerns, " \
-            "please contact IT Security." \
-            "" \
-            "<br><br><br><br>" \
-            "Bonjour, il y a une nouvelle vulnérabilité pour votre département." \
-            "<br>" \
-            "Sommaire de la vulnérabilité: " \
-            "<br>"\
-            "<strong>"+ vuln_desc +"</strong>" \
-            "<br>"\
-            "<a href="+cve_link_string+">Cliquez sur ce lien pour voir la vulnérabilité</a>" \
-            "<br>"\
-            "Ceci est un méssage automatisé, veuillez ne pas répondre s'il-vous-plait. Si vous avez des questions, " \
-            "veuillez contacté le département de Sécurité TI"
-
+    body = #body with HTML convention
     msg.attach(MIMEText(body, 'html'))
     print(msg)
 
@@ -51,4 +26,4 @@ def send_email(to_addr, vuln_desc, cve_link_string):
     server.quit()
 
 
-send_email("oag.windows@mailinator.com", "VULNERABILITY DESCRIPTION", cve_link_string)
+send_email("#email", "VULNERABILITY DESCRIPTION", cve_link_string)
