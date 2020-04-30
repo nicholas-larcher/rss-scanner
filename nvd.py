3cb5ac08398a52012f0a7004c4856d79a7087b24
Nicholas Larcher <40302942+nicholas-larcher@users.noreply.github.com>
2020-04-30 08:05:54 -0400

    Update nvd.py

--- nvd.py
+++ nvd.py
@@ -15,37 +15,13 @@ def send_email(to_addr, vuln_desc, cve_link_string):
 
     msg = MIMEMultipart()
     # sender email, password, receiver email and subject.
-    msg['From'] = 'oag.bvg.test@gmail.com'
-    password = '240Sparks'
+    msg['From'] = #email
+    password = #password
     msg['To'] = to_addr
-    msg['Subject'] = 'IT Security automated vulnerability monitoring / ' \
-                     'Système automatisé de surveillance des vulnérabilitées de Sécurité TI'
+    msg['Subject'] = #subject
 
     # body of email, string mixed with HTML
-    body =  "Le francais suit l'anglais" \
-            "<br><br>" \
-            "Hello, there is a new vulnerability for your department." \
-            "<br> " \
-            "Summary of the vulnerability:"\
-            "<br>"\
-            "<strong>" + vuln_desc + "</strong>" \
-            "<br>"\
-            "<a href="+cve_link_string+">Clink this link to view the vulnerability</a>" \
-            "<br>" \
-            "This is an automated message, please do not respond. If you have any questions or concerns, " \
-            "please contact IT Security." \
-            "" \
-            "<br><br><br><br>" \
-            "Bonjour, il y a une nouvelle vulnérabilité pour votre département." \
-            "<br>" \
-            "Sommaire de la vulnérabilité: " \
-            "<br>"\
-            "<strong>"+ vuln_desc +"</strong>" \
-            "<br>"\
-            "<a href="+cve_link_string+">Cliquez sur ce lien pour voir la vulnérabilité</a>" \
-            "<br>"\
-            "Ceci est un méssage automatisé, veuillez ne pas répondre s'il-vous-plait. Si vous avez des questions, " \
-            "veuillez contacté le département de Sécurité TI."
+    body =  #body html
 
     msg.attach(MIMEText(body, 'html'))
     print(msg)
